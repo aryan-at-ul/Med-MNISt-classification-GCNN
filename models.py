a867f9d08c4c1f72b4066efb423627fdb212a66f
Aryan Singh <aryan.singh@ul.ie>
2022-10-19 11:56:40 +0100

    I'm a lumberjack And I'm OK

---
+++ models.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+import tempfile
+import matplotlib.pyplot as plt
+import PIL
+import torch
+import numpy as np
+from sklearn.metrics import classification_report
+
+from monai.apps import download_and_extract
+from monai.config import print_config
+from monai.data import decollate_batch, DataLoader
+from monai.metrics import ROCAUCMetric
+from monai.networks.nets import DenseNet121
+from monai.networks.nets import ResNet
+from monai.networks.nets import EfficientNetBN
+from monai.transforms import (
+    Activations,
+    EnsureChannelFirst,
+    AsDiscrete,
+    Compose,
+    LoadImage,
+    RandFlip,
+    RandRotate,
+    RandZoom,
+    ScaleIntensity,
+)
+from monai.utils import set_determinism
